3c13e5e9e7bc03d79841a5d467d830ad206a8b26
Daniel Owens <owensdaniel8@yahoo.com>
2022-04-14 18:18:37 -0700

    commit tree 2b41da13cde45429096e0290ddedeffd2d62b0ac

---
+++ lib/song.rb
@@ -0,0 +1,35 @@
+require 'pry'
+
+
+
+class Song
+    attr_accessor :name, :artist, :genre
+   
+    @@count = 0
+    @@genres = []
+    @@artists = []
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@genres << genre
+        @@artists << artist
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.genres
+        @@genres
+    end
+
+    def self.artists
+        @@artists
+    end
+end
+
+ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "rap")
+
+binding.pry
